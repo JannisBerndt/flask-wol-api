895f80576ff83c1635a8126d399840b674277648
Jannis Berndt <berndtjannis@gmail.com>
2020-09-16 23:03:44 +0200

    better input data checking and error handling

--- wol.py
+++ wol.py
@@ -14,26 +14,39 @@ bp = Blueprint('wol', __name__, url_prefix='/wol')
 def wake():
     errors = ""
     try:
-        mac_address = request.form.get('mac-address').upper()
-        dst_ip = request.form.get('ip-address')
-        dst_port = request.form.get('port')
-        secureOn = str(request.form.get('secureon'))
-        app.logger.info("MAC: " + mac_address + ", IP: " + dst_ip + ", Port: " + dst_port + ", Password: " + secureOn)
+        mac_address = str(request.form.get('mac-address').upper() or None)
+        dst_ip = str(request.form.get('ip-address') or None)
+        dst_port = int(request.form.get('port') or None)
+        secureOn = str(request.form.get('secureon') or None)
+        app.logger.info("MAC: " + mac_address + ", IP: " + dst_ip + ", Port: " + str(dst_port) + ", Password: " + secureOn)
+    except Exception as e:
+        app.logger.error(e)
+        return json.dumps({
+            'message': 'Unable to parse input data!'
+        }), 400
 
-        if len(secureOn) != 6:
-            errors += "The SecureOn password has to be 6 characters long."
-        preset = Preset.query.filter_by(secureOn=secureOn).all()
-        if len(preset) > 1:
-            return json.dumps({
-                    'message': "The given password returns multiple presets!"
+    try:
+        # Use presets only if the password is the only given value. Else, every value apart from the optional secureOn has to be specifid. Otherwise its an error.
+        if mac_address is None and dst_ip is None and dst_port is None and secureOn is not None:
+            if len(secureOn) != 6:
+                return json.dumps({
+                    'message': "The SecureOn password has to be 6 characters long."
                 }), 400
-        preset = preset[0] if preset else None
-        app.logger.info(preset)
-        if preset is not None:
+            preset = Preset.query.filter_by(secureOn=secureOn).all()
+            if len(preset) > 1:
+                return json.dumps({
+                        'message': "The given password belongs to multiple presets! This is not allowed."
+                    }), 400
+            preset = preset[0] if preset else None
+            if preset is None:
+                return json.dumps({
+                        'message': "The given password does not match any preset! Please provide a correct password or all the information needed to send a packet without a preset."
+                    }), 400
+            app.logger.info('Preset: ' + preset)
             mac_address = preset["mac_address"]
             dst_ip = preset["ip_or_hostname"]
             dst_port = preset["port"]
-        else:
+        elif mac_address is not None and dst_ip is not None and dst_port is not None:
             if re.match(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$', mac_address) is None:
                 errors += "Invalid Mac Address. You need 12 Hex numbers and 5 colons. "
             if not "".join(dst_ip.split('.')).isnumeric():
@@ -41,18 +54,23 @@ def wake():
                     dst_ip = socket.gethostbyname(dst_ip)
                 except:
                     errors += "Unable to resolve Hostname."
-            dst_port = int(dst_port)
             if not -1 < dst_port < 65536 :
                 errors += "Invalid Port. Port has to be between 0 and 65535. "
+            if secureOn is not None and len(secureOn) != 6:
+                errors += "The SecureOn password has to be 6 characters long."
             if errors != "":
                 return json.dumps({
                     'message': errors
                 }), 400
+        else:
+            return json.dumps({
+                'message': 'You are missing some important information. Please either provide a valid MAC Adress, IP and Port or a secureOn password with nothing else.'
+            }), 400
+
         secureOn = secureOn.encode('ASCII').hex()
-        print(dst_ip, dst_port)
         mac_address = "".join(mac_address.split(':'))
         packetData = "FFFFFFFFFFFF" + "".join([mac_address]*16) + secureOn
-        print(packetData)
+        app.logger.info(packetData)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(bytearray.fromhex(packetData), (dst_ip, dst_port))
     except Exception as e:
