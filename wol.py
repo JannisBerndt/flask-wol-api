02abc126670d7ee3a3277f35c303497943aa8d7a
Jannis Berndt <berndtjannis@gmail.com>
2020-09-26 19:57:42 +0200

    some cleanup and renaming

--- wol.py
+++ wol.py
@@ -15,10 +15,10 @@ def wake():
     errors = ""
     try:
         mac_address = str(request.form.get('mac-address').upper())
-        dst_ip = str(request.form.get('ip-or-hostname'))
+        ip_or_hostname = str(request.form.get('ip-or-hostname'))
         dst_port = int(request.form.get('port')) if request.form.get('port') else None
         secureon = str(request.form.get('secureon'))
-        app.logger.info("MAC: " + mac_address + ", IP: " + dst_ip + ", Port: " + str(dst_port) + ", Password: " + secureon)
+        app.logger.info("MAC: " + mac_address + ", IP or Hostname: " + ip_or_hostname + ", Port: " + str(dst_port) + ", Password: " + secureon)
     except Exception as e:
         app.logger.error(e)
         return json.dumps({
@@ -26,39 +26,10 @@ def wake():
         }), 400
 
     try:
-        # Use presets only if the password is the only given value. Else, every value apart from the optional secureon has to be specified. Otherwise its an error.
-        if not mac_address and not dst_ip and not dst_port and secureon:
-            if len(secureon) != 6:
-                return json.dumps({
-                    'message': "The secureOn password has to be 6 characters long."
-                }), 400
-            preset = Preset.query.filter_by(secureon=secureon).all()
-            if len(preset) > 1:
-                return json.dumps({
-                        'message': "The given password belongs to multiple presets! This is not allowed."
-                    }), 400
-            preset = preset[0] if preset else None
-            if preset is None:
-                return json.dumps({
-                        'message': "The given password does not match any preset! Please provide a correct password or all the information needed to send a packet without a preset."
-                    }), 400
-            #app.logger.info('Preset: ' + preset)
-            mac_address = preset.mac_address
-            dst_ip = preset.ip_or_hostname
-            dst_port = preset.port
-            if not "".join(dst_ip.split('.')).isnumeric():
-                try:
-                    dst_ip = socket.gethostbyname(dst_ip)
-                except:
-                    errors += "Unable to resolve Hostname. "
-        elif mac_address and dst_ip and dst_port:
+        if mac_address and ip_or_hostname and dst_port:
             errors += validateMACAddress(mac_address)
-            if not "".join(dst_ip.split('.')).isnumeric():
-                try:
-                    dst_ip = socket.gethostbyname(dst_ip)
-                except:
-                    errors += "Unable to resolve Hostname. "
-            errors += validateIPAddress(dst_ip)
+            if "".join(ip_or_hostname.split('.')).isnumeric():
+                errors += validateIPAddress(ip_or_hostname)
             errors += validatePort(dst_port)
             if secureon and len(secureon) != 6:
                 errors += "The SecureOn password has to be 6 characters long."
@@ -70,18 +41,11 @@ def wake():
             return json.dumps({
                 'message': 'You are missing some important information. Please either provide a valid MAC Adress, IP and Port or a secureOn password with nothing else.'
             }), 400
-
-        secureon = secureon.encode('ASCII').hex()
-        mac_address = "".join(mac_address.split(':'))
-        packetData = "FFFFFFFFFFFF" + "".join([mac_address]*16) + secureon
-        app.logger.info(packetData)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(bytearray.fromhex(packetData), (dst_ip, dst_port))
+        sendMagicPacket(mac_address, ip_or_hostname, port, secureon)
     except Exception as e:
         app.logger.error(e)
-        errors += "Something went wrong. Please make sure you provided 1. a MAC Address with 6 blocks of two hex numbers separated by colons, 2. a valid IP address in dotted decimal notation or a hostname of you choice and 3. a port between 0 and 65535. Optionally you can also provide a secureOn password, which has to be 6 characters long."
         return json.dumps({
-            'message': errors
+            'message': e.args
         }), 400
     return json.dumps({
             'message': 'Magic Packet successfully sent.'
@@ -105,7 +69,7 @@ def wake_from_preset():
             'message': "The secureOn password has to be 6 characters long."
         }), 400
     preset = Preset.query.filter_by(secureon=secureon, name=name).all()
-    if len(preset) != 1:
+    if len(preset) > 1:
         return json.dumps({
             'message': "The given name and password belong to multiple presets! This is not allowed."
         }), 400
@@ -119,7 +83,7 @@ def wake_from_preset():
         sendMagicPacket(preset.mac_address, preset.ip_or_hostname, preset.port, secureon)
     except Exception as e:
         return json.dumps({
-            'message': e
+            'message': e.args
         }), 400
     return json.dumps({
         'message': 'Magic Packet successfully sent.'
